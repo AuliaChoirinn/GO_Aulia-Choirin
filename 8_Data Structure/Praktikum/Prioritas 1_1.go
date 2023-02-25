f56abd8c5a2b33860265ce449586c38db82acf89
AuliaChoirinn <125628617+AuliaChoirinn@users.noreply.github.com>
2023-02-25 20:42:55 +0700

    Update

---
+++ 8_Data Structure/Praktikum/Prioritas 1_1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ArrayMerge(arrayA, arrayB []string) []string {
+	result := make([]string, 0, len(arrayA)+len(arrayB))
+	result = append(result, arrayA...)
+	result = append(result, arrayB...)
+	return result
+}
+
+func main() {
+	// Test cases
+	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
+	// ["king", "devil jin", "akuma", "eddie", "steve", "geese"]
+
+	fmt.Println(ArrayMerge([]string{"sergei", "jin"}, []string{"jin", "steve", "bryan"}))
+	// ["sergei", "jin", "steve", "bryan"]
+
+	fmt.Println(ArrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
+	// ["alisa", "yoshimitsu", "devil jin", "law"]
+
+	fmt.Println(ArrayMerge([]string{}, []string{"devil jin", "sergoi"}))
+	// ["devil jin", "sergoi"]
+
+	fmt.Println(ArrayMerge([]string{"hwoarang"}, []string{}))
+	// ["hwoarang"]
+
+	fmt.Println(ArrayMerge([]string{}, []string{}))
+	// []
+}
